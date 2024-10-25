084a889fc9f42c146a2c6fd4ab2f92a282ee32b4
andrea260198 <andrea.cassotti@epfl.ch>
2024-10-25 22:59:08 +0200

    Function renamed

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def get_pseudo_rnd_sample(M: int) -> npt.NDArray[np.float64]:
     return normal_sample
 
 
-def calc_var(get_sample: Callable[[int], npt.NDArray[np.float64]], M: int, thresold: float) -> float:
+def calc_cutoff(get_sample: Callable[[int], npt.NDArray[np.float64]], M: int, thresold: float) -> float:
     sample = get_sample(M)
     p = sum(sample > thresold) / M
     return p
@@ -30,8 +30,8 @@ if __name__ == '__main__':
     threshold = 2.0
     MM = np.arange(10, 10_000, 100, dtype=int)
 
-    plt.plot(MM, list(map(lambda M: calc_var(get_pseudo_rnd_sample, M, threshold), MM)), "k")
-    plt.plot(MM, list(map(lambda M: calc_var(get_low_discr_sample, M, threshold), MM)), "r")
+    plt.plot(MM, list(map(lambda M: calc_cutoff(get_pseudo_rnd_sample, M, threshold), MM)), "k")
+    plt.plot(MM, list(map(lambda M: calc_cutoff(get_low_discr_sample, M, threshold), MM)), "r")
     plt.plot(MM, np.ones(MM.shape) * (1 - norm.cdf(threshold)), "k-")
     plt.legend(["Monte Carlo", "Quasi-Monte Carlo"])
     plt.show()
