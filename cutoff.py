95b7f6b4114bf4f87ccbfb93cc0dc76aaca1931a
andrea260198 <andrea.cassotti@epfl.ch>
2024-10-30 22:18:45 +0100

    Random seed defined and cutoff img updated

--- cutoff.py
+++ cutoff.py
@@ -1,7 +1,5 @@
-import numpy.random
-from scipy.stats import qmc
+from scipy.stats import norm, qmc
 import matplotlib.pyplot as plt
-from scipy.stats import norm
 import numpy as np
 import numpy.typing as npt
 from pipe import select
@@ -16,7 +14,8 @@ def get_low_discr_sample(M: int) -> npt.NDArray[np.float64]:
 
 
 def get_pseudo_rnd_sample(M: int) -> npt.NDArray[np.float64]:
-    normal_sample = numpy.random.normal(size=M)
+    np.random.seed(0)
+    normal_sample = np.random.normal(size=M)
     return normal_sample
 
 
@@ -27,10 +26,12 @@ def calc_cutoff(sample: npt.NDArray[np.float64], threshold: float) -> float:
 
 if __name__ == '__main__':
     threshold = 2.0
-    MM = range(10, 10_000, 100)
+    MM = range(50, 10_000, 100)
 
     plt.plot(MM, list(MM | select(get_pseudo_rnd_sample) | select(lambda sample: calc_cutoff(sample, threshold))), "k")
     plt.plot(MM, list(MM | select(get_low_discr_sample) | select(lambda sample: calc_cutoff(sample, threshold))), "r")
     plt.plot([MM[0], MM[-1]], 2 * [(1 - norm.cdf(threshold))], "k--")
     plt.legend(["Monte Carlo", "Quasi-Monte Carlo"])
+    plt.xlabel("M")
+    plt.ylabel("cutoff")
     plt.show()
